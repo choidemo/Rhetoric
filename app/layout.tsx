221906e0a61e0eba184b079f66476bd30c6eb006
choidemo <csh0504@naver.com>
2025-07-22 11:49:50 +0900

    refactor: Update app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import './globals.css';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'AI를 설득해라!',
-  description: 'AI와 토론하며 설득 능력을 키워보세요.',
+  title: 'Rhetoric',
+  description: 'AI 학생을 설득해보며 설득 능력을 키워보세요.',
 };
 
 export default function RootLayout({
